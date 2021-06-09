a7f780b61d399c6a81f51cd82b3e399ee3c29b8b
Danielle Thompson <danithompson74@gmail.com>
2021-06-08 18:25:41 -0700

    Write first test

---
+++ spec/ping_pong_spec.rb
@@ -0,0 +1,8 @@
+require('rspec')
+require('title_case')
+
+describe('#ping_pong') do
+  it("returns an array of ascending numbers up to the number entered") do
+    expect(ping_pong(2)).to(eq([1,2]))
+  end
+end

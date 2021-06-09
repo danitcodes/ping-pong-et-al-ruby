6ba50afa95e14de6830fea173eb77b3a031df01e
Danielle Thompson <danithompson74@gmail.com>
2021-06-08 20:43:21 -0700

    Purposely fail second test

--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -5,7 +5,10 @@ describe('#ping_pong') do
   it("returns an array of ascending numbers up to the number entered") do
     expect(ping_pong(2)).to(eq([1,2]))
   end
-  it("returns 'ping-pong' instead of of a number for numbers divisible by 3 & 5") do
-    expect(ping_pong(15)).to(eq([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, "ping-pong"]))
+  it("returns 'ping' for numbers divisible by 3") do
+    expect(ping_pong(6)).to(eq([1, 2, "ping", 4, 5, "ping"]))
   end
+  # it("returns 'ping-pong' instead of of a number for numbers divisible by 3 & 5") do
+  #   expect(ping_pong(15)).to(eq([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, "ping-pong"]))
+  # end
 end

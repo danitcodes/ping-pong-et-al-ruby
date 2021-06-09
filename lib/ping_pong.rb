b074dd3e98ba889b3543a64e6d7558b6b96d70e6
Danielle Thompson <danithompson74@gmail.com>
2021-06-08 19:32:57 -0700

    Purposely fail second test

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -4,7 +4,15 @@ def ping_pong(num)
   number = 0
   new_array = []
   num.times do |number|
-    new_array.push(number + 1)
+    # if # divisible by 3 & 5
+    #   new_array.push("ping-pong")
+    # elsif # divisible by 5
+    #   new_array.push("pong")
+    # elsif # divisible by 3
+    #   new_array.push("ping")
+    # else
+      new_array.push(number + 1)
+    #end
   end
   new_array
 end

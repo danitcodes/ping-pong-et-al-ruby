95fd0d10a3fb1f7f2fa7618e3021827f667180e8
Danielle Thompson <danithompson74@gmail.com>
2021-06-08 21:42:58 -0700

    WIP - add conditional for ping branch

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -3,16 +3,16 @@ require('pry')
 def ping_pong(num)
   number = 0
   new_array = []
-  num.times do |number|
-    # if # divisible by 3 & 5
-    #   new_array.push("ping-pong")
+  num.times do |x|
+    #if (number % 3 == 0) && (number % 5 == 0) # divisible by 3 & 5
+      #new_array.push("ping-pong")
     # elsif # divisible by 5
     #   new_array.push("pong")
-    # elsif # divisible by 3
-    #   new_array.push("ping")
-    # else
-      new_array.push(number + 1)
-    #end
+    if x % 3 == 0
+      new_array.push("ping")
+    else
+      new_array.push(x + 1)
+    end
   end
   new_array
 end

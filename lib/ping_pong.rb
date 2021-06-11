af2dfb42315374f9ce5afa4d4aee0492a366c8c5
Danielle Thompson <danithompson74@gmail.com>
2021-06-11 13:50:05 -0700

    Purposely pass refactor of 1st test w/ range

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,19 +1,17 @@
 require('pry')
 
 def ping_pong(num)
-  number = 0
-  new_array = []
-  num.times do |x|
-    number = x + 1
-    if number % 3 == 0 && number % 5 == 0
-      new_array.push("ping-pong")
-    elsif number % 5 == 0
-      new_array.push("pong")
-    elsif number % 3 == 0
-      new_array.push("ping")
-    else
-      new_array.push(number)
-    end
-  end
-  new_array
+  num_range = (1..num).to_a
+  num_range.each do |x|
+  #   if number % 3 == 0 && number % 5 == 0
+  #     num_range.push("ping-pong")
+  #   elsif number % 5 == 0
+  #     num_range.push("pong")
+  #   elsif number % 3 == 0
+  #     num_range.push("ping")
+  #   else
+  #     num_range.push(number)
+  #   end
+  # end
+  num_range
 end

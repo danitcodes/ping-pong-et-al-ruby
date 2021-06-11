768258cfc3f265b1db803224ce0ffbb8d4d06369
Danielle Thompson <danithompson74@gmail.com>
2021-06-11 09:12:02 -0700

    Fix failing ping test and purposely pass pong test too

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -4,14 +4,15 @@ def ping_pong(num)
   number = 0
   new_array = []
   num.times do |x|
+    number = x + 1
     #if (number % 3 == 0) && (number % 5 == 0) # divisible by 3 & 5
       #new_array.push("ping-pong")
-    # elsif # divisible by 5
-    #   new_array.push("pong")
-    if x % 3 == 0
+    if number % 5 == 0
+      new_array.push("pong")
+    elsif number % 3 == 0
       new_array.push("ping")
     else
-      new_array.push(x + 1)
+      new_array.push(number)
     end
   end
   new_array

5ba6b9b5957863a123b3392e839b7fd398e8a890
Danielle Thompson <danithompson74@gmail.com>
2021-06-11 09:10:42 -0700

    Purposely fail & pass ping and pong tests

--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -6,7 +6,10 @@ describe('#ping_pong') do
     expect(ping_pong(2)).to(eq([1,2]))
   end
   it("returns 'ping' for numbers divisible by 3") do
-    expect(ping_pong(6)).to(eq([1, 2, "ping", 4, 5, "ping"]))
+    expect(ping_pong(3)).to(eq([1, 2, "ping"]))
+  end
+  it("returns 'pong' for numbers divisible by 5") do
+    expect(ping_pong(10)).to(eq([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong"]))
   end
   # it("returns 'ping-pong' instead of of a number for numbers divisible by 3 & 5") do
   #   expect(ping_pong(15)).to(eq([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, "ping-pong"]))

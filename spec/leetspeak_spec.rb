5979f1524db2015b5754eb3341319ee0bdb558fd
Danielle Thompson <danithompson74@gmail.com>
2021-06-21 10:48:37 -0700

    Purposely fail first test

--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -2,7 +2,10 @@ require('rspec')
 require('leetspeak')
 
 describe('#leetspeak') do
-  it("does this thing") do
-    expect(leetspeak(x)).to(eq(y))
+  it('The letter "e" should be replaced with "3"') do
+    expect(leetspeak(e)).to(eq(3))
   end
+  # it('The letter "E" should be replaced with "3"') do
+  #   expect(leetspeak(E)).to(eq(3))
+  # end
 end

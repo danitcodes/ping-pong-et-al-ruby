15e2121f12446177bf0f88a4baf78140cef42a04
Danielle Thompson <danithompson74@gmail.com>
2021-06-21 14:58:22 -0700

    Purposely pass capital O test

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,30 +1,17 @@
 require('pry')
 
 def leetspeak(str)
-  #charAt or split string into individual characters
-  #loop each individual character
-  # conditional if charAt[x] = 'e'
-  # return 3
-  puts str
-#  str_array = []
   str_array = str.split("")
-  puts str_array
   leet_array = []
   str_array.each do |string_character|
-    puts str_array.class
-    puts string_character.class
-    puts string_character
-    puts str_array[0] == "e"
-    # puts string_character[0] == "e"
     if (string_character == 'e') | (string_character == "E")
       leet_array.push("3")
-    elsif (string_character == 'o')
+    elsif (string_character == 'o') | (string_character == "O")
       leet_array.push("0")
     else
       leet_array.push(string_character)
     end
   end
   joined = leet_array.join("")
-  #puts leet_array
   return joined
 end

c206e4b147fc0a881b9f76c40b983f04e4837bd1
Danielle Thompson <danithompson74@gmail.com>
2021-06-21 15:03:08 -0700

    Purposely pass capital I test

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -8,6 +8,8 @@ def leetspeak(str)
       leet_array.push("3")
     elsif (string_character == 'o') | (string_character == "O")
       leet_array.push("0")
+    elsif (string_character == "I")
+      leet_array.push("1")
     else
       leet_array.push(string_character)
     end

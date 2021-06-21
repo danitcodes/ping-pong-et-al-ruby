fcad65df29126c463dc47168bcf8efd797c6c2ef
Danielle Thompson <danithompson74@gmail.com>
2021-06-21 14:50:56 -0700

    Purposely fix and pass 2nd test

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -5,17 +5,21 @@ def leetspeak(str)
   #loop each individual character
   # conditional if charAt[x] = 'e'
   # return 3
-  str_array = []
-  str_array.push(str.split())
+  puts str
+#  str_array = []
+  str_array = str.split("")
+  puts str_array
   leet_array = []
   str_array.each do |string_character|
+    puts str_array.class
     puts string_character.class
     puts string_character
-    puts string_character[0] == "e"
-    if (string_character[0] == "e") | (string_character[0] == "E")
-      leet_array.push(3)
+    puts str_array[0] == "e"
+    # puts string_character[0] == "e"
+    if (string_character == 'e') | (string_character == "E")
+      leet_array.push("3")
     else
-      leet_array.push(string_character)
+      leet_array.push("a")
     end
   end
   joined = leet_array.join("")

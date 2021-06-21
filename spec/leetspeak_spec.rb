7ec48adbfbe56c288db9dc9f026783ec7276605b
Danielle Thompson <danithompson74@gmail.com>
2021-06-21 14:51:08 -0700

    Purpsoly fail third test for non-magic letters

--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -3,9 +3,12 @@ require('leetspeak')
 
 describe('String#leetspeak') do
   it('The letter e should be replaced with 3') do
-    expect(leetspeak("e")).to(eq("3"))
+    expect(leetspeak("eee")).to(eq("333"))
   end
   it('The letter "E" should be replaced with "3"') do
     expect(leetspeak("E")).to(eq("3"))
+  end
+  it('Any non-magic letter should stay the same') do
+    expect(leetspeak("bat")).to(eq("bat"))
   end
 end

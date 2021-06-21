bc205fd7df0ac5b2b7d7a42c8ba3a4f42f40dd43
Danielle Thompson <danithompson74@gmail.com>
2021-06-21 11:51:44 -0700

    Purposely fail 2nd test capital E

--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -5,7 +5,7 @@ describe('String#leetspeak') do
   it('The letter e should be replaced with 3') do
     expect(leetspeak("e")).to(eq("3"))
   end
-  # it('The letter "E" should be replaced with "3"') do
-  #   expect(leetspeak(E)).to(eq(3))
-  # end
+  it('The letter "E" should be replaced with "3"') do
+    expect(leetspeak("E")).to(eq("3"))
+  end
 end

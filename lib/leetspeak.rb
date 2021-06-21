7ac654149f7d2b6ec8bd8e00ffeedd8e33ccbe4f
Danielle Thompson <danithompson74@gmail.com>
2021-06-21 11:50:46 -0700

    Purposely pass first test

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,5 +1,24 @@
 require('pry')
 
-def leetspeak()
-  #code here
+def leetspeak(str)
+  #charAt or split string into individual characters
+  #loop each individual character
+  # conditional if charAt[x] = 'e'
+  # return 3
+  str_array = []
+  str_array.push(str.split())
+  leet_array = []
+  str_array.each do |string_character|
+    puts string_character.class
+    puts string_character
+    puts string_character[0] == "e"
+    if string_character[0] == "e"
+      leet_array.push(3)
+    else
+      leet_array.push(string_character)
+    end
+  end
+  joined = leet_array.join("")
+  #puts leet_array
+  return joined
 end
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -1,9 +1,9 @@
 require('rspec')
 require('leetspeak')
 
-describe('#leetspeak') do
-  it('The letter "e" should be replaced with "3"') do
-    expect(leetspeak(e)).to(eq(3))
+describe('String#leetspeak') do
+  it('The letter e should be replaced with 3') do
+    expect(leetspeak("e")).to(eq("3"))
   end
   # it('The letter "E" should be replaced with "3"') do
   #   expect(leetspeak(E)).to(eq(3))

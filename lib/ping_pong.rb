5e066f03a8cdac755311bd3a730cea750d060105
Danielle Thompson <danithompson74@gmail.com>
2021-06-21 19:06:53 -0700

    Return to previous working code

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,17 +1,19 @@
 require('pry')
 
 def ping_pong(num)
-  num_range = (1..num).to_a
-  num_range.each do |x|
-  #   if number % 3 == 0 && number % 5 == 0
-  #     num_range.push("ping-pong")
-  #   elsif number % 5 == 0
-  #     num_range.push("pong")
-  #   elsif number % 3 == 0
-  #     num_range.push("ping")
-  #   else
-  #     num_range.push(number)
-  #   end
-  # end
-  num_range
+  number = 0
+  new_array = []
+  num.times do |x|
+    number = x + 1
+    if number % 3 == 0 && number % 5 == 0
+      new_array.push("ping-pong")
+    elsif number % 5 == 0
+      new_array.push("pong")
+    elsif number % 3 == 0
+      new_array.push("ping")
+    else
+      new_array.push(number)
+    end
+  end
+  new_array
 end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -5,13 +5,13 @@ describe('#ping_pong') do
   it("returns an array of ascending numbers up to the number entered") do
     expect(ping_pong(2)).to(eq([1,2]))
   end
-  # it("returns 'ping' for numbers divisible by 3") do
-  #   expect(ping_pong(3)).to(eq([1, 2, "ping"]))
-  # end
-  # it("returns 'pong' for numbers divisible by 5") do
-  #   expect(ping_pong(10)).to(eq([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong"]))
-  # end
-  # it("returns 'ping-pong' instead of of a number for numbers divisible by 3 & 5") do
-  #   expect(ping_pong(15)).to(eq([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "ping-pong"]))
-  # end
+  it("returns 'ping' for numbers divisible by 3") do
+    expect(ping_pong(3)).to(eq([1, 2, "ping"]))
+  end
+  it("returns 'pong' for numbers divisible by 5") do
+    expect(ping_pong(10)).to(eq([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong"]))
+  end
+  it("returns 'ping-pong' instead of of a number for numbers divisible by 3 & 5") do
+    expect(ping_pong(15)).to(eq([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "ping-pong"]))
+  end
 end
